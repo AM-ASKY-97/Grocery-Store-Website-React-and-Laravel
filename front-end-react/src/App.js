2cff3b3d29a6759512a7d66180594dad60e9dd46
AM-ASKY-97 <am.asky97@gmail.com>
2023-07-04 15:03:46 +0530

    I added the home section

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header/Header';
+import Home from './component/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   );
 }
---
+++ front-end-react/src/component/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './Home.css';
+
+const Home = () => {
+    return (
+        <section className="home" id="home">
+            <div className="content">
+                <h3>buy best <span> organic product </span> online </h3>
+                <a href="#banner" className="btn"> get yours now </a>
+            </div>
+        </section>
+    )
+}
+
+export default Home
